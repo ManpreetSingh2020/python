ec32ffbfa546b79af2af3fed4b641aaf52a2e32e
ManpreetSingh2020 <Manpres3@gmail.com>
2021-03-09 09:03:28 +0530

    Clean string is to replace a substring/character in a supplied string

---
+++ cleanstring.py
@@ -0,0 +1,11 @@
+def cleanstring(string,substring):
+    if substring in string:
+        pos =string.find(substring)
+        return string[0:pos] + string[pos+len(substring):]
+    else:
+        return string
+
+if __name__ == '__main__' :
+    a = input("Enter a String:")
+    b = input("Enter a substring/character: ")
+    print(cleanstring(a , b ))
