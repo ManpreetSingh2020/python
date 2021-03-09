5671070f52fc9cb71be46abaec5b1069d9de369e
ManpreetSingh2020 <62993477+ManpreetSingh2020@users.noreply.github.com>
2021-03-09 08:41:29 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,4 +1,5 @@
 print('Welcome to Github')
 print('New Change in the code, this should reflect after commit and push to github')
 print('Second change in the code')
+print('Latest change in code via github, should come in pycharm using git fetch')
 
