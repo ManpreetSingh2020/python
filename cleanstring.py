6d8b493ecc8a718b1639d5e94005972ab322cc27
ManpreetSingh2020 <Manpres3@gmail.com>
2021-03-09 09:10:57 +0530

    improved readability as per PEP 8

--- cleanstring.py
+++ cleanstring.py
@@ -1,11 +1,12 @@
-def cleanstring(string,substring):
+def cleanstring(string, substring):
     if substring in string:
-        pos =string.find(substring)
+        pos = string.find(substring)
         return string[0:pos] + string[pos+len(substring):]
     else:
         return string
 
+
 if __name__ == '__main__' :
     a = input("Enter a String:")
     b = input("Enter a substring/character: ")
-    print(cleanstring(a , b ))
+    print(cleanstring(a, b))
